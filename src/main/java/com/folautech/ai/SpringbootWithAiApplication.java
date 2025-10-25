24ad7c390c6bf32b5c48cc7fc86b33424d5dd67a
Folau Kaveinga <folaukaveinga@gmail.com>
2025-10-24 20:27:21 -0600

    setup

---
+++ src/main/java/com/folautech/ai/SpringbootWithAiApplication.java
@@ -0,0 +1,13 @@
+package com.folautech.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folautech/ai/SpringbootWithAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folautech.ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
